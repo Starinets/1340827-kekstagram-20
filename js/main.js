0112ec7a7e2ea45791f1a3d0a25e53b5629f64ee
Starinets <tsn.ipad@gmail.com>
2020-06-07 20:46:29 +0600

    module3-task2

--- js/main.js
+++ js/main.js
@@ -19,6 +19,12 @@
     'Ульяна',
   ];
 
+  var PICTURES = document.querySelector('.pictures');
+  var TEMPLATE = document.querySelector('#picture').content.querySelector('.picture');
+  var TEMPLATE_IMAGE = TEMPLATE.querySelector('.picture__img');
+  var TEMPLATE_COMMENTS = TEMPLATE.querySelector('.picture__comments');
+  var TEMPLATE_LIKES = TEMPLATE.querySelector('.picture__likes');
+
   var generateUniqueArrayOfInteger = function (numberOfItems, minimum, maximum) {
     var arrayOfIntegers = [];
     var index = 0;
@@ -41,7 +47,9 @@
     var textMesssge = '';
 
     for (var index = 0; index < numberOfMessages; index++) {
-      textMesssge += MESSAGES[window.util.getRandomInteger(0, MESSAGES.length - 1)] + ' ';
+      textMesssge += MESSAGES[
+        window.util.getRandomInteger(0, MESSAGES.length - 1)
+      ] + ' ';
     }
 
     return textMesssge;
@@ -52,7 +60,6 @@
     var numberOfComments = window.util.getRandomInteger(1, 6);
 
     for (var index = 0; index < numberOfComments; index++) {
-
       photoComments.push(
           {
             avatar: 'img/avatar-' + window.util.getRandomInteger(1, 6) + '.svg',
@@ -61,50 +68,41 @@
           }
       );
     }
+
     return photoComments;
   };
 
-  // document.querySelector('.big-picture').classList.remove('hidden');
+  var generatePhotosData = function () {
+    var photos = [];
+
+    for (var index = 0; index < NUMBER_OF_ARRAY_ITEMS; index++) {
+      var item = {
+        url: 'photos/' + photoID[index] + '.jpg',
+        description: 'Случайная фотография',
+        likes: window.util.getRandomInteger(15, 200),
+        comments: getPhotoComments(),
+      };
+      photos.push(item);
+    }
+
+    return photos;
+  };
+
+  var renderPhotos = function (photos) {
+    var fragment = document.createDocumentFragment();
+
+    photos.forEach(function (photo) {
+      TEMPLATE_IMAGE.src = photo.url;
+      TEMPLATE_COMMENTS.textContent = photo.comments.length;
+      TEMPLATE_LIKES.textContent = photo.likes;
+      fragment.appendChild(TEMPLATE.cloneNode(true));
+    });
+
+    PICTURES.appendChild(fragment);
+  };
 
   var photoID = generateUniqueArrayOfInteger(NUMBER_OF_ARRAY_ITEMS, 1, 25);
-  var photos = [];
 
-  var numberOfComments = window.util.getRandomInteger(1, 20);
-
-  for (var index = 0; index < numberOfComments; index++) {
-    var item = {
-      avatar: 'img/avatar-6.svg',
-      message: 'В целом всё неплохо. Но не всё.',
-      name: 'Артем'
-    };
-
-  }
-
-  for (var index = 0; index < NUMBER_OF_ARRAY_ITEMS; index++) {
-    var item = {
-      url: 'photos/' + photoID[index] + '.jpg',
-      description: 'Случайная фотография',
-      likes: window.util.getRandomInteger(15, 200),
-      comments: getPhotoComments(),
-    };
-
-    photos.push(item);
-  }
-
-  var fragment = document.createDocumentFragment();
-  var template = document.querySelector('#picture').content.querySelector('.picture');
-  var img = template.querySelector('.picture__img');
-  var pictures = document.querySelector('.pictures');
-
-  photos.forEach(function (photo) {
-    img.src = photo.url;
-    var comments = template.querySelector('.picture__comments');
-    comments.textContent = photo.comments.length;
-    var likes = template.querySelector('.picture__likes');
-    likes.textContent = photo.likes;
-
-    fragment.appendChild(template.cloneNode(true));
-  });
-
-  pictures.appendChild(fragment);
+  var photos = generatePhotosData();
+  renderPhotos(photos);
 })();
